b312abdcd17ce5b1f33063dc1cd53a39013556c0
Tokenzzz <luisguilhermebini7@gmail.com>
2026-07-27 20:10:23 -0300

    feat: adiciona script inicial main.py na pasta src

---
+++ devJR/src/main.py
@@ -0,0 +1,8 @@
+def menu():
+    print("=== SISTEMA DE CADASTRO DEVJR ===")
+    print("1. Cadastrar novo contato")
+    print("2. Listar contatos")
+    print("3. Sair")
+
+if __name__ == "__main__":
+    menu()
